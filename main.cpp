36c058ccb66b6e0f25d82c916a41d8a57b9ea239
BenjaminVerdier <ben.verdier@hotmail.fr>
2021-03-04 18:36:49 -0500

    Add imgui support and add loader

--- main.cpp
+++ main.cpp
@@ -1,35 +1,39 @@
+// igl includes
 #include <igl/opengl/glfw/Viewer.h>
+#include <igl/read_triangle_mesh.h>
+#include <igl/file_dialog_open.h>
 
+//imgui includes
+#include <igl/opengl/glfw/imgui/ImGuiMenu.h>
+#include <imgui.h>
+
+Eigen::MatrixXd V;
+Eigen::MatrixXi F;
+
+void customize_menu(igl::opengl::glfw::Viewer& viewer);
 
 int main(int argc, char *argv[])
 {
-  // Inline mesh of a cube
-  const Eigen::MatrixXd V= (Eigen::MatrixXd(8,3)<<
-    0.0,0.0,0.0,
-    0.0,0.0,1.0,
-    0.0,1.0,0.0,
-    0.0,1.0,1.0,
-    1.0,0.0,0.0,
-    1.0,0.0,1.0,
-    1.0,1.0,0.0,
-    1.0,1.0,1.0).finished();
-  const Eigen::MatrixXi F = (Eigen::MatrixXi(12,3)<<
-    1,7,5,
-    1,3,7,
-    1,4,3,
-    1,2,4,
-    3,8,7,
-    3,4,8,
-    5,7,8,
-    5,8,6,
-    1,5,6,
-    1,6,2,
-    2,6,8,
-    2,8,4).finished().array()-1;
+    // Plot the mesh
+    igl::opengl::glfw::Viewer viewer;
+    customize_menu(viewer);
+}
 
-  // Plot the mesh
-  igl::opengl::glfw::Viewer viewer;
-  viewer.data().set_mesh(V, F);
-  viewer.data().set_face_based(true);
-  viewer.launch();
+void customize_menu(igl::opengl::glfw::Viewer& viewer)
+{
+	igl::opengl::glfw::imgui::ImGuiMenu menu;
+	viewer.plugins.push_back(&menu);
+    menu.callback_draw_viewer_menu = [&]()
+    {
+        if (ImGui::Button("Load Mesh"))
+        {
+            std::string filename = igl::file_dialog_open();
+            if (!filename.empty() && igl::read_triangle_mesh(filename, V, F))
+            {
+                viewer.data().set_mesh(V, F);
+                viewer.core().align_camera_center(V, F);
+            }
+        }
+    };
+    viewer.launch();
 }
